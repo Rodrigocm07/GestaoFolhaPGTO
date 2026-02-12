eb88ac815ca5d93d02ad558fa3294a71b8a98c82
RODRIGO CARVAHO MACHADO <05065940096@senacrs.edu.br>
2026-02-12 16:52:45 -0300

    Classe funcionario da main

---
+++ GestaoDePagamento/src/Gestao/Funcionario.java
@@ -0,0 +1,37 @@
+package Gestao;
+
+public abstract class Funcionario {
+
+    private String nome;
+    private String CPF;
+    private double salarioBase;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+
+    public void setSalarioBase(double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
+
+    public void calcularPagamento() {
+        //Sera implementado futuramente;
+    }
+
+}
